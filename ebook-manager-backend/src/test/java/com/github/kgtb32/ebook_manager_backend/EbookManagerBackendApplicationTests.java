5f942d687393b6d05b843a742adefb13f2e5827d
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-06-14 18:32:57 +0200

    Init backend project

---
+++ ebook-manager-backend/src/test/java/com/github/kgtb32/ebook_manager_backend/EbookManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kgtb32.ebook_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
