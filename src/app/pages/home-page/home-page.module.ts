4fe0e651a63216bb598da75df8d91844aca234a8
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-06-15 07:28:52 +0200

    Add Layout Module and Header component

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
+import { LayoutModule } from '../../components/layout/layout.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomePageComponent } from './home-page.component';
     HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule
   ]
 })
 export class HomePageModule { }
