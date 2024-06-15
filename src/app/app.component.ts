c3a1d1d96bae542734e978b6c488279217e533f9
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-06-15 07:07:06 +0200

    Removed default boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { invoke } from "@tauri-apps/api/tauri";
 import { Command } from '@tauri-apps/api/shell';
+import { invoke } from "@tauri-apps/api/tauri";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   greetingMessage = "";
