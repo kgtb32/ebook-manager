5c048c885fd476242195f70a806cf19dd21b3eaf
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-06-18 22:05:35 +0200

    Updated PDF load API

--- ebook-manager-backend/src/main/java/com/github/kgtb32/ebook_manager_backend/controllers/PDFController.java
+++ ebook-manager-backend/src/main/java/com/github/kgtb32/ebook_manager_backend/controllers/PDFController.java
@@ -2,11 +2,11 @@ package com.github.kgtb32.ebook_manager_backend.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.github.kgtb32.ebook_manager_backend.models.in.PDFLoadMetadata;
 import com.github.kgtb32.ebook_manager_backend.models.out.PDFInformation;
 import com.github.kgtb32.ebook_manager_backend.services.PDFService;
 
@@ -21,7 +21,7 @@ public class PDFController {
     }
 
     @PostMapping("/load")
-    public ResponseEntity<PDFInformation> loadPDF(@RequestBody PDFLoadMetadata pdfMetadata){
-        return ResponseEntity.ok(pdfService.loadPDF(pdfMetadata));
+    public ResponseEntity<PDFInformation> loadPDF(@RequestParam("file") MultipartFile file){
+        return ResponseEntity.ok(pdfService.loadPDF(file));
     }
 }
--- ebook-manager-backend/src/main/java/com/github/kgtb32/ebook_manager_backend/models/out/PDFInformation.java
+++ ebook-manager-backend/src/main/java/com/github/kgtb32/ebook_manager_backend/models/out/PDFInformation.java
@@ -3,6 +3,7 @@ package com.github.kgtb32.ebook_manager_backend.models.out;
 import com.github.kgtb32.ebook_manager_backend.models.interfaces.PDFLoad;
 
 public record PDFInformation(
+    String id,
     boolean isValid,
     int maxPages
 ) implements PDFLoad {}
