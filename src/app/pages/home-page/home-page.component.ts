223538f450b18101bb76c3d2215077f442636713
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-06-21 21:45:46 +0200

    Add PDF page change feature

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PdfService } from '../../services/pdf/pdf-service.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  image?: SafeUrl
 
+  constructor(private pdfService: PdfService, private sanitizer: DomSanitizer) {
+    this.pdfService.documentChanged.subscribe({
+      next: () => this.loadThumbnail()
+    })
+    this.pdfService.paginationChanged.subscribe({
+      next: page => this.loadThumbnail(page)
+    })
+  }
+
+  loadThumbnail(page: number = 0) {
+    this.pdfService.loadPDFThumbnail(page).subscribe({
+      next: imageBlob => {
+        this.image = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(imageBlob));
+      }
+    })
+  }
 }
